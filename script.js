03a49cb6eae91204ac4c0541663f831b735db451
Basia Niedzielczyk <66676830+BasiaNie@users.noreply.github.com>
2020-12-12 10:19:58 +0100

    username

--- script.js
+++ script.js
@@ -1,20 +1,5 @@
-var age = -65;
-
-console.log(Boolean(age));
-
-if (!Boolean(age)) {
-    console.log('Falsy value provided!');
-} else if (age > 60) {
-    colsole.log('Age is more than 60');
-} else if (age < 0) {
-    console.log('Age is wrong');
-}
-else if (age < 60 && age > 0) {
-    console.log('Age is less than 60');
-}  else {
-    console.log('Age is 60')
+function displayMessage(username) {
+    console.log('Hello, ' + username)
 }
 
-if (age !== 100) {
-    console.log('It is not 100')
-}
+displayMessage('Mike');
