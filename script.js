8dca57dea89d3f50359dd1bb79fa65d2a4fea14e
Basia Niedzielczyk <66676830+BasiaNie@users.noreply.github.com>
2020-12-12 11:38:21 +0100

    loop while and for

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
 function showResult() {
   var counter = 0;
+  var i;
 
-  while (counter !== 10) {
-    console.log(counter);
+//   while (counter !== 10) {
+//     console.log(counter);
 
-    counter = counter + 1;
-  }
+//     counter = counter + 1;
+//   }
+
+    for (i = 0; i < 10; i = i + 4) {
+        console.log(i);
+    }
+
 }
+
+
+
 
